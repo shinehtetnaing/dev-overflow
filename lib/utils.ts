5d44d3346151bf01f795124a8d961b86b186557d
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-11-09 23:46:38 +0630

    util functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,44 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const now = new Date();
+  const diffInMilliseconds: number = Number(now) - Number(createdAt);
+
+  const seconds = Math.floor(diffInMilliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
+
+  if (seconds < 60) {
+    return rtf.format(-seconds, "second");
+  } else if (minutes < 60) {
+    return rtf.format(-minutes, "minute");
+  } else if (hours < 24) {
+    return rtf.format(-hours, "hour");
+  } else if (days < 7) {
+    return rtf.format(-days, "day");
+  } else if (weeks < 4) {
+    return rtf.format(-weeks, "week");
+  } else if (months < 12) {
+    return rtf.format(-months, "month");
+  } else {
+    return rtf.format(-years, "year");
+  }
+};
+
+export const formatNumber = (number: number): string => {
+  if (Math.abs(number) >= 1e6) {
+    return (number / 1e6).toFixed(1) + "M";
+  } else if (Math.abs(number) >= 1e3) {
+    return (number / 1e3).toFixed(1) + "K";
+  } else {
+    return number.toString();
+  }
+};
