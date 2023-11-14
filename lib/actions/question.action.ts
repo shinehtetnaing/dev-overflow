7a0ccf886d53d06d4ab5f220bbab1d9e9965fb75
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-11-14 23:26:20 +0630

    create question

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,10 +1,38 @@
 "use server";
 
+import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
 
 export async function createQuestion(params: any) {
   try {
     // connect to db
     connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+
+    // create question
+    const question = await Question.create({ title, content, author });
+
+    const tagDocuments = [];
+
+    // create tag or get them if they already exist
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true }
+      );
+
+      tagDocuments.push(existingTag._id);
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
+
+    // Create an interaction record for the user's ask_question action
+
+    // Increment author's reputation by +5 for creating a question
   } catch (error) {}
 }
