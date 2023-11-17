ce6f9676f46d9ce1161ed4ee6b9ad73a7c360291
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-11-17 22:56:47 +0630

    display questions on home page

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,10 +1,29 @@
 "use server";
 
-import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
+import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
+import User from "@/database/user.model";
+import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
 
-export async function createQuestion(params: any) {
+export async function getQuestions(params: GetQuestionsParams) {
+  try {
+    connectToDatabase();
+
+    const questions = await Question.find({})
+      .populate({ path: "tags", model: Tag })
+      .populate({ path: "author", model: User })
+      .sort({ createdAt: -1 });
+
+    return { questions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createQuestion(params: CreateQuestionParams) {
   try {
     // connect to db
     connectToDatabase();
@@ -34,5 +53,7 @@ export async function createQuestion(params: any) {
     // Create an interaction record for the user's ask_question action
 
     // Increment author's reputation by +5 for creating a question
+
+    revalidatePath(path);
   } catch (error) {}
 }
