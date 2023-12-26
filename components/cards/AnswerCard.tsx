50ef6722416b03b84fd20b9d5f29b3aa502f5a8a
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-12-26 23:40:14 +0630

    fix edit-delete question-answer component

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -48,7 +48,7 @@ const AnswerCard = ({
 
         <SignedIn>
           {showActionButtons && (
-            <EditDeleteAction type="Answers" itemId={JSON.stringify(_id)} />
+            <EditDeleteAction type="Answer" itemId={JSON.stringify(_id)} />
           )}
         </SignedIn>
       </div>
