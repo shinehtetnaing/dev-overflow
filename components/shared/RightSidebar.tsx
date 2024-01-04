15d332b591841970c636eaf4ba37c98205c0aa48
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-01-04 22:07:25 +0630

    show top questions

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,15 +1,7 @@
+import { getHotQuestions } from "@/lib/actions/question.action";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import RenderTag from "./RenderTag";
-
-const hotQuestions = [
-  { _id: "1", title: "How do I use express as a custom server in NextJs?" },
-  { _id: "2", title: "How do I use express as a custom server in NextJs?" },
-  { _id: "3", title: "How do I use express as a custom server in NextJs?" },
-  { _id: "4", title: "How do I use express as a custom server in NextJs?" },
-  { _id: "5", title: "How do I use express as a custom server in NextJs?" },
-];
 
 const popularTags = [
   { _id: "1", name: "javascript", totalQuestions: 5 },
@@ -19,7 +11,9 @@ const popularTags = [
   { _id: "5", name: "laravel", totalQuestions: 10 },
 ];
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -27,7 +21,7 @@ const RightSidebar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
